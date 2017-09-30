255672aaaf9111bb5e4dee230e5edd19d7557c28
Redhatfour1 <yaloumessenger@yahoo.com>
2017-09-30 13:23:11 -0700

    finished lab 8

--- TwitterClient/app/src/main/java/com/example/louis/twitterclient/HomeTimelineActivity.java
+++ TwitterClient/app/src/main/java/com/example/louis/twitterclient/HomeTimelineActivity.java
@@ -1,5 +1,6 @@
 package com.example.louis.twitterclient;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -10,6 +11,10 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
+import com.twitter.sdk.android.tweetui.UserTimeline;
 
 import java.util.ArrayList;
 
@@ -24,22 +29,61 @@ public class HomeTimelineActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Twitter.initialize(this);
-        Log.d(TAG, "on Create: Twitter Insrance - " + Twitter.getInstance());
-
         setContentView(R.layout.activity_home_timeline);
 
-        setupListView();
+        Twitter.initialize(this);
+        Log.d(TAG, "on Create: Twitter Instance - " + Twitter.getInstance());
+        TwitterSession session = TwitterCore.getInstance().getSessionManager().getActiveSession();
+//        TwitterCore.getInstance().getSessionManager().clearActiveSession();
+
+        Log.d(TAG, "onCreate: Session " + session);
+        
+        if(session != null) {
+//            setupListView();
+            setupUserTimelinelist();
+        } else {
+            Intent loginIntent = new Intent(this, LoginActivity.class);
+            startActivity(loginIntent);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        setupListView();
+        setupUserTimelinelist();
+    }
+    private void setupUserTimelinelist() {
+        UserTimeline userTimeline = new UserTimeline.Builder().build();
+        TweetTimelineListAdapter userTimelineAdapter = new TweetTimelineListAdapter(this, userTimeline);
+        twitterListView = (ListView) findViewById(R.id.twitter_list_view);
+        twitterListView.setAdapter(userTimelineAdapter);
     }
 
     private void setupListView() {
 
         twitterListView = (ListView) findViewById(R.id.twitter_list_view);
-        ArrayList<LHTweet> allTweets = LHJson.getTweets(this, true);
-        TweetListAdapter adapter = new TweetListAdapter(allTweets);
+        final ArrayList<LHTweet> allFetchedTweets = LHJson.getTweets(this, true);
+        TweetListAdapter adapter = new TweetListAdapter(allFetchedTweets) {
+
+            @Override
+            public View getView(int i, View view, ViewGroup viewGroup) {
+                LHTweet currentTweet = (LHTweet) getItem(i);
+
+                view = getLayoutInflater().inflate(R.layout.tweet_list_item, null);
+                TextView usernameView = (TextView) view.findViewById(R.id.textView_username);
+                TextView tweetTextView = (TextView) view.findViewById(R.id.textView_tweet_text);
+                TextView locationView = (TextView) view.findViewById(R.id.textView_location);
+                usernameView.setText(currentTweet.user.name);
+                tweetTextView.setText(currentTweet.text);
+                locationView.setText(currentTweet.user.location);
+
+                return view;
+            }
+        };
         twitterListView.setAdapter(adapter);
     }
-    class TweetListAdapter extends BaseAdapter {
+    abstract class TweetListAdapter extends BaseAdapter {
         private ArrayList<LHTweet> allTweets;
 
         public TweetListAdapter(ArrayList<LHTweet> allTweets){
@@ -60,20 +104,6 @@ public class HomeTimelineActivity extends AppCompatActivity {
         @Override
         public long getItemId(int i) {
             return 0;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            LHTweet currentTweet = allTweets.get(i);
-            view = getLayoutInflater().inflate(R.layout.tweet_list_item, null);
-            TextView usernameView = (TextView) view.findViewById(R.id.textView_username);
-            TextView tweetTextView = (TextView) view.findViewById(R.id.textView_tweet_text);
-            TextView locationView = (TextView)  view.findViewById(R.id.textView_location);
-            usernameView.setText(currentTweet.user.name);
-            tweetTextView.setText(currentTweet.text);
-            locationView.setText(currentTweet.user.location);
-
-            return view;
         }
     }
 }
